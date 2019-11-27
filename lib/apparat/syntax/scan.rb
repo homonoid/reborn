76527bdff277e48518c4969e9c9a3f92e066850a
Alexey Yurchenko <homonoidian@yandex.ru>
2019-11-27 21:36:12 +0300

    Able to scan text literals and understand interpolation.

--- lib/apparat/syntax/scan.rb
+++ lib/apparat/syntax/scan.rb
@@ -16,6 +16,7 @@ module Apparat
       @source = source
       @line, @column = 1, 1 # line and column are for humans, so start with 1
       @pos = 0
+      @string, @interpolation = false, false
     end
 
     private def makeToken(type, value, length = value.size)
@@ -23,28 +24,57 @@ module Apparat
     end
 
     private def consume(chunk)
-      case chunk
-      when /\A(;|=>?|<=|>=|\+=|\-=|\*=|\^=|,|\.|[\(\{\[\]\)\}\]\|])/
-        makeToken($1.to_sym, $1)
-      when /\A([\+\-\*\/\^><])/
-        makeToken(:OP, $1)
-      when /\A([a-zA-Z_][a-zA-Z0-9_]+|[a-zA-Z])/
-        makeToken(@@KEYWORDS[$1] || :ID, $1)
-      when /\A0(b)([01]+)/, /\A0(x|u)([0-9A-fa-f]+)/, /0(o)([0-7]+)/
-        type = {'b' => :BIN, 'x' => :HEX, 'o' => :OCT, 'u' => :UNI}[$1]
-        makeToken(type, $2, $&.size) # value has no 0[boux], but length does, so use the whole match
-      when /\A([0-9]+\.[0-9])(e\-?[0-9]+)?/
-        $2 ? makeToken(:SCI, $&) : makeToken(:FLOAT, $1)
-      when /\A([1-9][0-9]*|0)/
-        makeToken(:DECIMAL, $1)
-      when /\A[\n]+/
-        @line += $&.size # skip multiple newlines a time
-        @column = 1
-        makeToken(:IGNORE, nil, $&.size)
-      when /\A[ \t\r]+/
-        makeToken(:IGNORE, nil, $&.size)
+      if not @string
+        case chunk
+        when /\A(;|=>?|<=|>=|\+=|\-=|\*=|\^=|,|\.|[\(\{\[\]\)\]\|])/
+          makeToken($1.to_sym, $1)
+        when /\A\}/
+          if @interpolation
+            @string = true
+            @interpolation = false
+            makeToken(:TINTER, '}')
+          else
+            makeToken(:"}", '}')
+          end
+        when /\A([\+\-\*\/\^><])/
+          makeToken(:OP, $1)
+        when /\A([a-zA-Z_][a-zA-Z0-9_]+|[a-zA-Z])/
+          makeToken(@@KEYWORDS[$1] || :ID, $1)
+        when /\A0(b)([01]+)/, /\A0(x|u)([0-9A-fa-f]+)/, /0(o)([0-7]+)/
+          type = {'b' => :BIN, 'x' => :HEX, 'o' => :OCT, 'u' => :UNI}[$1]
+          makeToken(type, $2, $&.size) # value has no 0[boux], but length does, so use the whole match
+        when /\A([0-9]+\.[0-9])(e\-?[0-9]+)?/
+          $2 ? makeToken(:SCI, $&) : makeToken(:FLOAT, $1)
+        when /\A([1-9][0-9]*|0)/
+          makeToken(:DECIMAL, $1)
+        when /\A[\n]+/
+          @line += $&.size # skip multiple newlines a time
+          @column = 1
+          makeToken(:IGNORE, nil, $&.size)
+        when /\A[ \t\r]+/
+          makeToken(:IGNORE, nil, $&.size)
+        when /\A"/
+          @string = true
+          makeToken(:QUOTE, '"')
+        else
+          raise ApparatError.new("panic of '#{chunk[0]}'", @line, @column)
+        end
       else
-        raise ApparatError.new("panic because of '#{chunk[0]}'", @line, @column)
+        case chunk
+        when /\A([^\n"\\\{]|\\[ntvr])/
+          makeToken(:TCHAR, $1)
+        when /\A\{/
+          @string = false
+          @interpolation = true
+          makeToken(:BINTER, '{')
+        when /\A\}/
+          makeToken(:TINTER, '}')
+        when /\A"/
+          @string = false
+          makeToken(:QUOTE, '"')
+        else
+          raise ApparatError.new("panic of '#{chunk[0]}' in text literal", @line, @column)
+        end
       end
     end
 
