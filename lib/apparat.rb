a75dde87b6c7b777733e08a6ad996c333a2938f4
Alexey Yurchenko <homonoidian@yandex.ru>
2019-11-27 21:50:02 +0300

    Able to scan simple examples. Entry supports files.

--- lib/apparat.rb
+++ lib/apparat.rb
@@ -1,12 +1,44 @@
-require_relative 'apparat/syntax/scan'
-require 'readline'
+# BE AWARE:
+# Current revision of this file is DEBUG and DEBUG only.
 
-while line = Readline.readline('~ ', true)
+require_relative 'apparat/syntax/scan'
+
+def scan(source)
   begin
-    Apparat::Scanner.new(line).scan.each do |token|
+    Apparat::Scanner.new(source).scan.each do |token|
       puts "#{token.type}\t#{token.value}"
     end
   rescue Apparat::ApparatError => e
     puts "=== SORRY! ===\n #{e}"
   end
 end
+
+def repl
+  require 'readline'
+
+  while line = Readline.readline('~ ', true)
+    scan(line)
+  end
+end
+
+def file(name)
+  source = open(name, 'r').read
+  scan(source)
+end
+
+def main(args)
+  if args.empty?
+    repl
+  else
+    args.each do |filename|
+      begin
+        file(filename)
+      rescue Errno::ENOENT
+        puts "=== SORRY! ===\n File '#{filename}' does not exist"
+        exit(1) # exit with error
+      end
+    end
+  end
+end
+
+main(ARGV)
